e212b12c73ed202dc2e7dcf403d27debdd844739
Jan Cannaerts <jcn8jx2@admin.colruyt.int>
2020-10-29 18:59:10 +0100

    First cleanup of pooling.

--- pooling.go
+++ pooling.go
@@ -1,50 +1,38 @@
 package go_ibm_db
 
 import (
+	"Time"
 	"database/sql"
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 //DBP struct type contains the timeout, dbinstance and connection string
 type DBP struct {
 	sql.DB
-	con string
-	n   time.Duration
+	con  string
+	n    time.Duration
+	pool *Pool
 }
 
 //Pool struct contais the about the pool like size, used and available connections
 type Pool struct {
 	availablePool map[string][]*DBP
 	usedPool      map[string][]*DBP
-	PoolSize      int
+	poolSize      int
+	mu            sync.Mutex
 }
 
-var b *Pool
-var ConnMaxLifetime, PoolSize int
-
 //Pconnect will return the pool instance
-func Pconnect(PoolSize string) *Pool {
-	var Size int
-	count := len(PoolSize)
-	if count > 0 {
-		opt := strings.Split(PoolSize, "=")
-		if opt[0] == "PoolSize" {
-			Size, _ = strconv.Atoi(opt[1])
-		} else {
-			fmt.Println("Not a valid parameter")
-		}
-	} else {
-		Size = 100
-	}
+func Pconnect(poolSize int) *Pool {
 	p := &Pool{
 		availablePool: make(map[string][]*DBP),
 		usedPool:      make(map[string][]*DBP),
-		PoolSize:      Size,
+		poolSize:      poolSize,
 	}
-	b = p
 
 	return p
 }
@@ -55,23 +43,29 @@ var Psize int
 //Open will check for the connection in the pool
 //If not opens a new connection and stores in the pool
 func (p *Pool) Open(Connstr string, options ...string) *DBP {
-	var Time time.Duration
-	count := len(options)
-	if count > 0 {
-		for i := 0; i < count; i++ {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	var time time.Duration
+
+	//Check if there are options
+	if len(options) > 0 {
+
+		for i := 0; i < len(options); i++ {
 			opt := strings.Split(options[i], "=")
 			if opt[0] == "SetConnMaxLifetime" {
-				ConnMaxLifetime, _ = strconv.Atoi(opt[1])
-				Time = time.Duration(ConnMaxLifetime) * time.Second
+				time = Time.Duration(strconv.Atoi(opt[1])) * time.Second
 			} else {
 				fmt.Println("not a valid parameter")
 			}
 		}
 	} else {
-		Time = 30 * time.Second
+		time = 30 * Time.Second
 	}
-	if Psize < p.PoolSize {
+
+	if Psize < p.poolSize {
 		Psize = Psize + 1
+
 		if val, ok := p.availablePool[Connstr]; ok {
 			if len(val) > 1 {
 				dbpo := val[0]
@@ -80,13 +74,13 @@ func (p *Pool) Open(Connstr string, options ...string) *DBP {
 				val = val[:len(val)-1]
 				p.availablePool[Connstr] = val
 				p.usedPool[Connstr] = append(p.usedPool[Connstr], dbpo)
-				dbpo.SetConnMaxLifetime(Time)
+				dbpo.SetConnMaxLifetime(time)
 				return dbpo
 			} else {
 				dbpo := val[0]
 				p.usedPool[Connstr] = append(p.usedPool[Connstr], dbpo)
 				delete(p.availablePool, Connstr)
-				dbpo.SetConnMaxLifetime(Time)
+				dbpo.SetConnMaxLifetime(time)
 				return dbpo
 			}
 		} else {
@@ -94,34 +88,45 @@ func (p *Pool) Open(Connstr string, options ...string) *DBP {
 			if err != nil {
 				return nil
 			}
+
 			dbi := &DBP{
-				DB:  *db,
-				con: Connstr,
-				n:   Time,
+				DB:   *db,
+				con:  Connstr,
+				n:    Time,
+				pool: p,
 			}
+
 			p.usedPool[Connstr] = append(p.usedPool[Connstr], dbi)
-			dbi.SetConnMaxLifetime(Time)
+			dbi.SetConnMaxLifetime(time)
+
 			return dbi
 		}
+
 	} else {
 		db, err := sql.Open("go_ibm_db", Connstr)
 		if err != nil {
 			return nil
 		}
+
 		dbi := &DBP{
-			DB:  *db,
-			con: Connstr,
+			DB:   *db,
+			con:  Connstr,
+			pool: p,
 		}
+
 		return dbi
 	}
 }
 
 //Close will make the connection available for the next release
 func (d *DBP) Close() {
+	d.pool.mu.Lock()
+	defer d.pool.mu.Unlock()
+
 	Psize = Psize - 1
 	var pos int
 	i := -1
-	if valc, okc := b.usedPool[d.con]; okc {
+	if valc, okc := d.pool.usedPool[d.con]; okc {
 		if len(valc) > 1 {
 			for _, b := range valc {
 				i = i + 1
@@ -133,12 +138,12 @@ func (d *DBP) Close() {
 			copy(valc[pos:], valc[pos+1:])
 			valc[len(valc)-1] = nil
 			valc = valc[:len(valc)-1]
-			b.usedPool[d.con] = valc
-			b.availablePool[d.con] = append(b.availablePool[d.con], dbpc)
+			d.pool.usedPool[d.con] = valc
+			d.pool.availablePool[d.con] = append(d.pool.availablePool[d.con], dbpc)
 		} else {
 			dbpc := valc[0]
-			b.availablePool[d.con] = append(b.availablePool[d.con], dbpc)
-			delete(b.usedPool, d.con)
+			d.pool.availablePool[d.con] = append(d.pool.availablePool[d.con], dbpc)
+			delete(d.pool.usedPool, d.con)
 		}
 		go d.Timeout()
 	} else {
@@ -148,11 +153,14 @@ func (d *DBP) Close() {
 
 //Timeout for closing the connection in pool
 func (d *DBP) Timeout() {
+	d.pool.mu.Lock()
+	defer d.pool.mu.Unlock()
+
 	var pos int
 	i := -1
 	select {
 	case <-time.After(d.n):
-		if valt, okt := b.availablePool[d.con]; okt {
+		if valt, okt := d.pool.availablePool[d.con]; okt {
 			if len(valt) > 1 {
 				for _, b := range valt {
 					i = i + 1
@@ -164,12 +172,12 @@ func (d *DBP) Timeout() {
 				copy(valt[pos:], valt[pos+1:])
 				valt[len(valt)-1] = nil
 				valt = valt[:len(valt)-1]
-				b.availablePool[d.con] = valt
+				d.pool.availablePool[d.con] = valt
 				dbpt.DB.Close()
 			} else {
 				dbpt := valt[0]
 				dbpt.DB.Close()
-				delete(b.availablePool, d.con)
+				delete(d.pool.availablePool, d.con)
 			}
 		}
 	}
@@ -177,6 +185,9 @@ func (d *DBP) Timeout() {
 
 //Release will close all the connections in the pool
 func (p *Pool) Release() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
 	if p.availablePool != nil {
 		for _, vala := range p.availablePool {
 			for _, dbpr := range vala {
@@ -199,5 +210,5 @@ func (p *Pool) Release() {
 func (p *Pool) Display() {
 	fmt.Println(p.availablePool)
 	fmt.Println(p.usedPool)
-	fmt.Println(p.PoolSize)
+	fmt.Println(p.poolSize)
 }
